528e191cec122231f6e20de5b79ad46a702c0447
IT <ledinh.quang1997@gmail.com>
2018-09-01 14:40:26 +0700

    Refactor Map imple to abstract ID gen. closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -20,14 +20,14 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+
         owner1.setFirstName("Quang");
         owner1.setLastName("Le");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+
         owner2.setFirstName("Quill");
         owner2.setLastName("JS");
 
@@ -35,14 +35,13 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+
         vet1.setFirstName("Quang");
         vet1.setLastName("Le");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Quill");
         vet2.setLastName("JS");
 
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+    private final OwnerService ownerService;
+
+    public VetController(VetService vetService, OwnerService ownerService) {
+        this.vetService = vetService;
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/vets","vets/index","vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
